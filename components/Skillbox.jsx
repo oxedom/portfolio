1253010216b7d437482cd38805e731a95b956439
sam <oxedom@gmail.com>
2023-04-05 18:39:25 +0300

    skillbox change;

--- components/Skillbox.jsx
+++ components/Skillbox.jsx
@@ -1,8 +1,8 @@
 const Skillbox = ({ children, title }) => {
   return (
-    <section className="shadow">
+    <section className=" ">
       <h3 className="text-center text-3xl font-bold mb-6"> {title} </h3>
-      <div className="bg-gray-100  rounded-lg aspect-square  p-5 grid grid-cols-2  gap-2">
+      <div className="bg-gray-100  rounded-lg aspect-square shadow p-5 grid grid-cols-2  gap-2">
         {children}
       </div>
     </section>
